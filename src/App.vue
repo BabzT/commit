e982c2b42e70233ef5937383b4db095ee9a5c2a5
BabzT <babatumdehezekiah7@gmail.com>
2022-06-29 14:46:29 +0100

    come

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png">
+  <h2>Hello Tunde</h2>
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
 </template>
 
 <script>
